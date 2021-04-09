148e190baf1b93bf2dff35178d069eb69fcdcdb5
scorpion11133 <scorpion11133@gmail.com>
2021-04-09 08:34:39 +0300

    add hometask

--- src/index.js
+++ src/index.js
@@ -29,13 +29,34 @@
 // 1. Вы готовы войти?
 // да => Введите ваше имя => Ваше имя: {{ИМЯ}}
 // нет => Вы не вошли
-const ready = confirm("Вы готовы войти?");
+// const ready = confirm("Вы готовы войти?");
 
-if (ready) {
-  const name = prompt("Введите ваше имя");
+// if (ready) {
+//   const name = prompt("Введите ваше имя");
 
-  console.log(`Ваше имя: ${name}`);
-  //   console.log("Ваше имя: " + name);
-} else {
-  console.log("Вы не вошли");
+//   console.log(`Ваше имя: ${name}`);
+//   //   console.log("Ваше имя: " + name);
+// } else {
+//   console.log("Вы не вошли");
+// }
+
+// ДОМАШКА
+const number = prompt("Введите число от 1 до 10");
+
+let answer = prompt("Какое число загадал игрок 1?");
+
+function test() {
+  if (answer === number) {
+    console.log("Да! Вы угадали число");
+  } else {
+    console.log("Нет. Попробуйте еще разок :)");
+    answer = prompt("Какое число загадал игрок 1?");
+    test();
+  }
+}
+
+test();
+
+function tratata() {
+  tratata();
 }
